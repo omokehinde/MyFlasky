5e88be806de960b8df08cc8f3dba19de58fd0c42
omokehinde <engr.omokehinde@gmail.com>
2020-02-18 13:05:19 +0100

    rich text posts

--- app/models.py
+++ app/models.py
@@ -5,6 +5,8 @@ from . import db
 from . import login_manager
 from flask_login import UserMixin, AnonymousUserMixin
 from datetime import datetime
+from markdown import markdown
+import bleach
 import hashlib
 
 class Permission:
@@ -72,7 +74,19 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    body_html = db.Column(db.Text)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    @staticmethod
+    def on_changed_body(target, value, oldvalue, initiator):
+        allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code',
+                        'em', 'i', 'li', 'ol', 'pre', 'strong', 'ul',
+                        'h1', 'h2', 'h3', 'p']
+        target.body_html = bleach.linkify(bleach.clean(
+                            markdown(value, output_format='html'),
+                            tags=allowed_tags, strip=True))
+
+db.event.listen(Post.body, 'set', Post.on_changed_body)
 
 class User(UserMixin,db.Model):
     __tablename__ = 'users'
