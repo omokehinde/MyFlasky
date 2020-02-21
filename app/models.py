3423f32e07e17999b2073831dcc7bd8c8a977cff
omokehinde <engr.omokehinde@gmail.com>
2020-02-21 13:14:57 +0100

    user self-follow

--- app/models.py
+++ app/models.py
@@ -132,7 +132,16 @@ class User(UserMixin,db.Model):
                 self.role = Role.query.filter_by(default=True).first()
         if self.email is not None and self.avatar_hash is None:
             self.avatar_hash = self.gravatar_hash()
+        self.follow(self)
     
+    @staticmethod
+    def add_self_follows():
+        for user in User.query.all():
+            if not user.is_following(user):
+                user.follow(user)
+                db.session.add(user)
+                db.session.commit()
+
     @property
     def password(self):
         raise AttributeError('password is not a readable attribute')
