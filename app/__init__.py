6cced1cecc5adeb08319e1461fa1609a19586b01
omokehinde <engr.omokehinde@gmail.com>
2020-03-04 20:09:53 +0100

    handle api errors

--- app/__init__.py
+++ app/__init__.py
@@ -30,6 +30,8 @@ def create_app(config_name):
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
     from .auth import auth as auth_blueprint
+    from .api import api as api_blueprint
     # attach routes and custom error pages here
+    app.register_blueprint(api_blueprint, url_prefix='/api/v1')
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
     return app
